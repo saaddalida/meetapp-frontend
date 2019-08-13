0f95ade6d279f7cd2a02f49bc5d4ae4dd8d001de
saaddalida <dalidassaad@gmail.com>
2019-08-13 17:29:52 -0300

    Cadastro na aplicaçao

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,21 +3,44 @@ import { takeLatest, all, call, put } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSucess } from './actions';
+import { signInSucess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSucess(token, user));
+    yield put(signInSucess(token, user));
 
-  history.push('/dashboard');
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+  }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
+
+    history.push('/');
+  } catch (err) {
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
