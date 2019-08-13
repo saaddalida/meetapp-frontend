04cb7cc993aa119fea967becbc28c437e4a5287c
saaddalida <dalidassaad@gmail.com>
2019-08-13 16:42:33 -0300

    Autenticaçao

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSucess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  yield put(signInSucess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
