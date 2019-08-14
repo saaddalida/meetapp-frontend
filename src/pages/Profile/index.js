1fa19a3bffb21125a0302009bd0c16357d6399b3
saaddalida <dalidassaad@gmail.com>
2019-08-13 21:25:48 -0300

    Update perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  const [oldPassword, setOldPassword] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
@@ -16,12 +31,26 @@ export default function Profile() {
         <Input name="email" placeholder="Enter your e-mail" />
 
         <hr />
-        <Input type="password" name="oldPassword" placeholder="Old password" />
-        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="oldPassword"
+          value={oldPassword}
+          placeholder="Old password"
+          onChange={e => setOldPassword(e.target.value)}
+        />
+        <Input
+          type="password"
+          name="password"
+          value={password}
+          placeholder="New password"
+          onChange={e => setPassword(e.target.value)}
+        />
         <Input
           type="password"
           name="confirmPassword"
+          value={confirmPassword}
           placeholder="Confirm your password"
+          onChange={e => setConfirmPassword(e.target.value)}
         />
 
         <button type="submit">Update profile</button>
