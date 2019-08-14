09dc61bded2c65a414ab63a5978d55b96a421bd1
saaddalida <dalidassaad@gmail.com>
2019-08-14 19:23:26 -0300

    Criando pagina detalhes

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import pt from 'date-fns/locale/pt';
+import { format, parseISO } from 'date-fns';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import history from '~/services/history';
 
-export default function Details() {
-  return <div />;
+import { Container, Meetup } from './styles';
+
+export default function Details({ match }) {
+  const { id } = match.params;
+  const [meetup, setMeetup] = useState([]);
+
+  useEffect(() => {
+    async function loadMeetup() {
+      const response = await api.get(`meetups/${id}/details`);
+      setMeetup({
+        ...response.data,
+        url: response.data.File.url,
+        formattedDate: format(
+          parseISO(response.data.date),
+          "d 'de' MMMM', às' HH'h' mm'min'",
+          {
+            locale: pt,
+          }
+        ),
+      });
+    }
+
+    loadMeetup();
+  }, [id]);
+
+  function handleEdit() {
+    history.push(`/meetup/${id}/edit`);
+  }
+
+  async function handleCancel() {
+    await api.delete(`meetup/${id}`);
+    history.push('/dashboard');
+  }
+
+  return (
+    <Container>
+      <header>
+        <strong>{meetup.title}</strong>
+        {!meetup.past && (
+          <div>
+            <button type="button" onClick={handleEdit} className="edit">
+              Editar
+            </button>
+            <button type="button" onClick={handleCancel} className="cancel">
+              Cancelar
+            </button>
+          </div>
+        )}
+      </header>
+
+      <Meetup>
+        <img src={meetup.url} alt="meetapp" />
+        <p>{meetup.description}</p>
+        <p>
+          Caso queira participar como palestrante do meetup envie um e-mail para
+          organizacao@meetapp.com.br.
+        </p>
+        <div>
+          <span>{meetup.formattedDate}</span>
+          <span>{meetup.location}</span>
+        </div>
+      </Meetup>
+    </Container>
+  );
 }
+
+Details.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,3 +1,76 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 900px;
+  margin: 50px auto;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 25px;
+
+    strong {
+      font-size: 32px;
+      color: #fff;
+    }
+
+    button {
+      width: 120px;
+      height: 40px;
+      letter-spacing: 0.5px;
+      color: #fff;
+      font-weight: bold;
+      border: 0;
+      margin-right: 10px;
+      border-radius: 4px;
+
+      &.edit {
+        background: #4dbaf9;
+        &:hover {
+          background: ${darken(0.05, '#4DBAF9')};
+        }
+      }
+      &.cancel {
+        background: #d44059;
+        &:hover {
+          background: ${darken(0.05, '#D44059')};
+        }
+      }
+    }
+  }
+`;
+
+export const Meetup = styled.div`
+  width: 900px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    align-self: center;
+    width: 500px;
+    height: 250px;
+    margin-bottom: 15px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #ffffff;
+    line-height: 32px;
+    margin-bottom: 20px;
+  }
+
+  div {
+    font-size: 16px;
+    color: #ffffff;
+    opacity: 0.6;
+
+    span {
+      margin-right: 60px;
+    }
+  }
+`;
